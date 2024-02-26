3bf4f64e5abe32b2c1a246b390a4bdc2a194b275
obid12 <obidiaramdhani23@gmail.com>
2024-02-26 07:21:44 +0700

    on progress

--- app/src/main/java/com/obidia/testagrii/presentation/inputdata/ListSubNotesAdapter.kt
+++ app/src/main/java/com/obidia/testagrii/presentation/inputdata/ListSubNotesAdapter.kt
@@ -1,7 +1,5 @@
 package com.obidia.testagrii.presentation.inputdata
 
-import android.text.Editable
-import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -12,8 +10,10 @@ import androidx.recyclerview.widget.RecyclerView
 import com.obidia.testagrii.databinding.ItemSubNoteBinding
 import com.obidia.testagrii.domain.model.SubNoteModel
 import com.obidia.testagrii.presentation.inputdata.ListSubNotesAdapter.SubNoteViewHolder
+import com.obidia.testagrii.utils.visible
 
-class ListSubNotesAdapter : ListAdapter<SubNoteModel, SubNoteViewHolder>(DiffCallBack) {
+class ListSubNotesAdapter(val isListUser: Boolean = false) :
+  ListAdapter<SubNoteModel, SubNoteViewHolder>(DiffCallBack) {
 
   private var onDeleteListener: ((item: SubNoteModel) -> Unit)? = null
   private var onUpdateListener: ((item: SubNoteModel, text: String) -> Unit)? = null
@@ -27,10 +27,19 @@ class ListSubNotesAdapter : ListAdapter<SubNoteModel, SubNoteViewHolder>(DiffCal
   }
 
   override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SubNoteViewHolder {
+    val view = ItemSubNoteBinding.inflate(
+      LayoutInflater.from(parent.context), parent, false
+    )
+//    if (isListUser) {
+//      view.root.isFocusable = false
+//      view.root.isFocusableInTouchMode = false
+//      view.root.requestFocus()
+//      parent.isFocusable = false
+//      parent.isFocusableInTouchMode = false
+//      parent.requestFocus()
+//    }
     return SubNoteViewHolder(
-      ItemSubNoteBinding.inflate(
-        LayoutInflater.from(parent.context), parent, false
-      )
+      view
     )
   }
 
@@ -44,34 +53,31 @@ class ListSubNotesAdapter : ListAdapter<SubNoteModel, SubNoteViewHolder>(DiffCal
   ) : RecyclerView.ViewHolder(binding.root) {
     fun bind(data: SubNoteModel) {
       binding.run {
+        btnCheckBox.isEnabled = !isListUser
+        tvNoteBody.let {
+          it.text = data.text
+          it.visible(isListUser)
+        }
         etNoteBody.let {
+          it.visible(!isListUser)
           it.setText(data.text)
-//          it.onFocusChangeListener = getFocusWatcher(it, data)
-          it.addTextChangedListener(getCharacterWatcher(it, data))
+          it.onFocusChangeListener = getFocusWatcher(it, data)
         }
-        ivDelete.setOnClickListener {
-          onDeleteListener?.invoke(data)
+        ivDelete.let {
+          it.setOnClickListener {
+            onDeleteListener?.invoke(data)
+          }
+          it.visible(!isListUser)
         }
       }
     }
   }
 
-  private fun getCharacterWatcher(editText: EditText, item: SubNoteModel): TextWatcher {
-    return object : TextWatcher {
-      override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
-      override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
-      override fun afterTextChanged(p0: Editable?) {
-        onUpdateListener?.invoke(item, editText.text.toString())
-      }
+  private fun getFocusWatcher(editText: EditText, item: SubNoteModel): View.OnFocusChangeListener {
+    return View.OnFocusChangeListener { _, _ ->
+      onUpdateListener?.invoke(item, editText.text.toString())
     }
   }
-
-//  private fun getFocusWatcher(editText: EditText, item: SubNoteModel): View.OnFocusChangeListener {
-//    return View.OnFocusChangeListener { _, _ ->
-//      if (!editText.hasFocus())
-//        onUpdateListener?.invoke(item, editText.text.toString())
-//    }
-//  }
 
   object DiffCallBack : DiffUtil.ItemCallback<SubNoteModel>() {
     override fun areItemsTheSame(
